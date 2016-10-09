b1060960aad6db8718fb855367021ce56e5d3050
cumig0t <cumigot@gmail.com>
2016-10-10 03:30:48 +0700

    Add project files.

---
+++ LoginPattern/LoginPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace LoginPattern
+{
+	public class LoginPage : ContentPage
+	{
+		Entry username, password;
+		public LoginPage (ILoginManager ilm)
+		{
+			var button = new Button { Text = "Login" };
+			button.Clicked += (sender, e) => {
+				if (String.IsNullOrEmpty(username.Text) || String.IsNullOrEmpty(password.Text))
+				{
+					DisplayAlert("Validation Error", "Username and Password are required", "Re-try");
+				} else {
+					// REMEMBER LOGIN STATUS!
+					App.Current.Properties["IsLoggedIn"] = true;
+					ilm.ShowMainPage();
+				}
+			};
+			var create = new Button { Text = "Create Account" };
+			create.Clicked += (sender, e) => {
+				MessagingCenter.Send<ContentPage> (this, "Create");
+			};
+
+			username = new Entry { Text = "" };
+			password = new Entry { Text = "" };
+			Content = new StackLayout {
+				Padding = new Thickness (10, 40, 10, 10),
+				Children = {
+					new Label { Text = "Login", FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) }, 
+					new Label { Text = "Username" },
+					username,
+					new Label { Text = "Password" },
+					password,
+					button, create
+				}
+			};
+		}
+	}
+}
+
